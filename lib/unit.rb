cbe6fbe2d8fb49bdb6cdf3a2f1bc866c67ec945c
cege1808 <gunawan.clarissa@hotmail.com>
2016-06-30 00:06:36 +0000

    add can_attack?, is_dead? methods

--- lib/unit.rb
+++ lib/unit.rb
@@ -7,15 +7,31 @@ class Unit
     @attack_power = attack_power
   end
 
-
-  def attack!(enemy_unit)
-    enemy_unit.damage(@attack_power)
+  def is_alive?
+    !is_dead?
   end
 
 
-  def damage(enemy_AP) 
-    @health_points -= enemy_AP
+  def is_dead?
+    health_points <= 0
   end
 
+  # If self is alive and enemy is alive
+  def attack!(enemy)
+    if can_attack?(enemy)
+      enemy.damage(self)
+      true
+    else
+      false
+    end
+  end
+
+  def can_attack?(enemy)
+    is_alive? && enemy.is_alive?
+  end
+
+  def damage(enemy) 
+    @health_points -= enemy.attack_power
+  end
 
 end
